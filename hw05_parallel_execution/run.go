48e9a173a55804fec0f109768c7bb1a2ef9c011c
Evgeny Bryzgalov <bryzgalovevgeny@7gis.ru>
2021-07-23 16:55:49 +0500

    First Commit

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,14 +2,73 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
+type Pool struct {
+	Tasks          []Task
+	workersCount   int
+	maxErrorsCount int
+	tasksChan      chan Task
+	wg             sync.WaitGroup
+	mu             sync.Mutex
+	errCount       int
+}
+
+func NewPool(tasks []Task, workersCount int, maxErrorsCount int) *Pool {
+	return &Pool{
+		Tasks:          tasks,
+		workersCount:   workersCount,
+		maxErrorsCount: maxErrorsCount,
+		tasksChan:      make(chan Task),
+	}
+}
+
+func (p *Pool) Run() error {
+	for i := 0; i < p.workersCount; i++ {
+		go p.work()
+	}
+
+	for _, task := range p.Tasks {
+		// put task im queue if not enough errors
+
+		p.wg.Add(1)
+		p.tasksChan <- task
+
+	}
+
+	close(p.tasksChan)
+
+	p.wg.Wait()
+	if p.errCount >= p.maxErrorsCount {
+		return ErrErrorsLimitExceeded
+	}
+	return nil
+}
+
+func (p *Pool) work() {
+	for task := range p.tasksChan {
+		p.mu.Lock()
+		continueAdd := p.errCount < p.maxErrorsCount
+		p.mu.Unlock()
+		if continueAdd {
+			if err := task(); err != nil {
+				p.mu.Lock()
+				p.errCount++
+				p.mu.Unlock()
+
+			}
+		}
+		p.wg.Done()
+	}
+}
+
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
-	return nil
+	p := NewPool(tasks, n, m)
+	return p.Run()
 }
