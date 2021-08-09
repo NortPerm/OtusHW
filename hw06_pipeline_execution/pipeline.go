89d4f4c7c9260fee2fef24bd492888cd6f7ba3a2
Bryzgalov <aepKeallah@gmail.com>
2021-08-09 11:38:45 +0500

    HW06

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -8,7 +8,49 @@ type (
 
 type Stage func(in In) (out Out)
 
+type Pool struct {
+	stages   []Stage
+	in, done In
+	out      Bi
+}
+
+func NewPool(stages []Stage, in In, done In) *Pool {
+	return &Pool{
+		stages: stages,
+		in:     in,
+		done:   done,
+		out:    make(Bi),
+	}
+}
+
+func (p *Pool) execute() Out {
+	go func() {
+		defer close(p.out)
+		for {
+			select {
+			case value, ok := <-p.in:
+				if ok {
+					p.out <- value
+				} else {
+					return
+				}
+			case <-p.done:
+				return
+			}
+		}
+	}()
+
+	return p.out
+}
+
+func (p *Pool) composeStages() {
+	for _, stage := range p.stages {
+		p.in = stage(p.in)
+	}
+}
+
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	p := NewPool(stages, in, done)
+	p.composeStages()
+	return p.execute()
 }
