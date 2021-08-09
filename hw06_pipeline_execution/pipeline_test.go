e3566ca27b0a84f2fce8e0b7a746f3fcd404a9e3
Bryzgalov <aepKeallah@gmail.com>
2021-08-09 12:06:15 +0500

    HW06

--- hw06_pipeline_execution/pipeline_test.go
+++ hw06_pipeline_execution/pipeline_test.go
@@ -28,98 +28,82 @@ func TestPipeline(t *testing.T) {
 			return out
 		}
 	}
-
 	stages := []Stage{
 		g("Dummy", func(v interface{}) interface{} { return v }),
 		g("Multiplier (* 2)", func(v interface{}) interface{} { return v.(int) * 2 }),
 		g("Adder (+ 100)", func(v interface{}) interface{} { return v.(int) + 100 }),
 		g("Stringifier", func(v interface{}) interface{} { return strconv.Itoa(v.(int)) }),
 	}
-
 	t.Run("simple case", func(t *testing.T) {
 		in := make(Bi)
 		data := []int{1, 2, 3, 4, 5}
-
 		go func() {
 			for _, v := range data {
 				in <- v
 			}
 			close(in)
 		}()
-
 		result := make([]string, 0, 10)
 		start := time.Now()
 		for s := range ExecutePipeline(in, nil, stages...) {
 			result = append(result, s.(string))
 		}
 		elapsed := time.Since(start)
-
 		require.Equal(t, []string{"102", "104", "106", "108", "110"}, result)
 		require.Less(t,
 			int64(elapsed),
 			// ~0.8s for processing 5 values in 4 stages (100ms every) concurrently
 			int64(sleepPerStage)*int64(len(stages)+len(data)-1)+int64(fault))
 	})
-
 	t.Run("done case", func(t *testing.T) {
 		in := make(Bi)
 		done := make(Bi)
 		data := []int{1, 2, 3, 4, 5}
-
 		// Abort after 200ms
 		abortDur := sleepPerStage * 2
 		go func() {
 			<-time.After(abortDur)
 			close(done)
 		}()
-
 		go func() {
 			for _, v := range data {
 				in <- v
 			}
 			close(in)
 		}()
-
 		result := make([]string, 0, 10)
 		start := time.Now()
 		for s := range ExecutePipeline(in, done, stages...) {
 			result = append(result, s.(string))
 		}
 		elapsed := time.Since(start)
-
 		require.Len(t, result, 0)
 		require.Less(t, int64(elapsed), int64(abortDur)+int64(fault))
 	})
-
 	zeroStages := []Stage{
 		g("Dummy", func(v interface{}) interface{} { return v }),
 	}
-
 	t.Run("one stage", func(t *testing.T) {
 		in := make(Bi)
 		data := []int{1, 2, 3, 4, 5}
-
 		go func() {
 			for _, v := range data {
 				in <- v
 			}
 			close(in)
 		}()
-
 		result := make([]int, 0, 10)
 		start := time.Now()
 		for s := range ExecutePipeline(in, nil, zeroStages...) {
 			result = append(result, s.(int))
 		}
 		elapsed := time.Since(start)
-
 		require.Equal(t, []int{1, 2, 3, 4, 5}, result)
 		require.Less(t,
 			int64(elapsed),
 			// ~0.8s for processing 5 values in 4 stages (100ms every) concurrently
 			int64(sleepPerStage)*int64(len(zeroStages)+len(data)-1)+int64(fault))
 	})
-
 	i := 0
 	factorial := 1
 	for i < 5 {
@@ -129,25 +113,21 @@ func TestPipeline(t *testing.T) {
 		zeroStages = append(zeroStages, g("Square", func(v interface{}) interface{} { return v.(int) * j }))
 
 	}
-
 	t.Run("multy stage", func(t *testing.T) {
 		in := make(Bi)
 		data := []int{1, 2, 3, 4, 5}
-
 		go func() {
 			for _, v := range data {
 				in <- v
 			}
 			close(in)
 		}()
-
 		result := make([]int, 0, 10)
 		start := time.Now()
 		for s := range ExecutePipeline(in, nil, zeroStages...) {
 			result = append(result, s.(int))
 		}
 		elapsed := time.Since(start)
-
 		require.Equal(t, []int{factorial * 1, factorial * 2, factorial * 3, factorial * 4, factorial * 5}, result)
 		require.Less(t,
 			int64(elapsed),
