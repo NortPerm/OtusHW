4dc8dee98e1274fd5cdd21ef82ea6d51e1b45b70
Bryzgalov <aepKeallah@gmail.com>
2021-08-09 12:24:56 +0500

    add tests

--- hw06_pipeline_execution/pipeline_test.go
+++ hw06_pipeline_execution/pipeline_test.go
@@ -90,4 +90,32 @@ func TestPipeline(t *testing.T) {
 		require.Len(t, result, 0)
 		require.Less(t, int64(elapsed), int64(abortDur)+int64(fault))
 	})
+	zeroStages := []Stage{
+		g("Dummy", func(v interface{}) interface{} { return v }),
+	}
+
+	t.Run("one stage", func(t *testing.T) {
+		in := make(Bi)
+		data := []int{1, 2, 3, 4, 5}
+
+		go func() {
+			for _, v := range data {
+				in <- v
+			}
+			close(in)
+		}()
+
+		result := make([]int, 0, 10)
+		start := time.Now()
+		for s := range ExecutePipeline(in, nil, zeroStages...) {
+			result = append(result, s.(int))
+		}
+		elapsed := time.Since(start)
+
+		require.Equal(t, []int{1, 2, 3, 4, 5}, result)
+		require.Less(t,
+			int64(elapsed),
+			// ~0.8s for processing 5 values in 4 stages (100ms every) concurrently
+			int64(sleepPerStage)*int64(len(zeroStages)+len(data)-1)+int64(fault))
+	})
 }
